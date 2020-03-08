3dbe331d17353061f275a8fdbf8137f02370b3c1
Katerina Gakhova <gakhovakaterina16@gmail.com>
2020-03-08 16:28:41 +0300

    добавила server.py и папку templates с index.html, не получается взять данные о времени из формы html

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, Response
+import cgi
 app = Flask(__name__)
 
 @app.route('/')
+def start_page():
+    title = 'Отслеживание машин'
+    header1 = 'Отслеживание машин Ситимобил в указанной локации'
+    return render_template('index.html', page_title = title, header1 = header1)
 
-def begin():
-    return "Привет!"
-
+@app.route("/forward/", methods=['POST'])
+def get_dt():
+    dt_form = cgi.FieldStorage()
+    dt_start = dt_form.getvalue("dt_start_html")
+    return dt_start
+    
 if __name__ == "__main__":
     app.run(debug=True)
